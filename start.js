8fdfdd90ce3502f4a57d6672f2de21d87823cf73
tech927 <onnui688@gmail.com>
2025-08-23 21:05:42 -0300

    Create start.js

---
+++ start.js
@@ -0,0 +1,11 @@
+const { spawn } = require('child_process')
+
+function startBot() {
+    const bot = spawn('node', ['index.js'], { stdio: 'inherit' })
+    bot.on('close', () => {
+        console.log('Bot stopped. Restarting...')
+        startBot()
+    })
+}
+
+startBot()
